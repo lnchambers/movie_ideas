ce0547be56765d1cbc12de69f09ac7ec516894b0
Luke Chambers <lukenatchambers@gmail.com>
2018-02-11 09:01:28 -0700

    Build out form for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
   def edit
+    @movie = Movie.find(params[:id])
   end
 
   def new
---
+++ spec/features/movies/user_can_edit_a_movie_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "As a registered User" do
+  before :each do
+    @movie = create(:movie)
+  end
+  describe "when I visit the movie/:id/edit page" do
+    it "I can edit a movie idea that I created" do
+      visit edit_movie_path(@movie)
+      fill_in "movie[title]", with: "Eupanifahorious Goes To Mars"
+      fill_in "movie[description]", with: "Read the title"
+      fill_in "movie[rating]", with: 10
+      click_on "Update Movie"
+
+      expect(current_path).to eq(movie_path(@movie))
+      expect(page).to have_content("Eupanifahorious Goes To Mars")
+      expect(page).to have_content("Read the title")
+      expect(page).to have_content("10")
+    end
+  end
+end
