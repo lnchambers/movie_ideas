dec2a75ac078e3213b873ad6612d0817cd804757
Luke Chambers <lukenatchambers@gmail.com>
2018-02-10 18:09:11 -0700

    Build out movie tests

---
+++ db/migrate/20180211005956_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.text :title
+      t.text :description
+      t.integer :rating
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/movies.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :movie do
+    title "MyText"
+    description "MyText"
+    rating 1
+  end
+end
