a0fd4c2714da10663b507ecec1be40359576ad7f
Luke Chambers <lukenatchambers@gmail.com>
2018-02-11 16:45:34 -0700

    Build out validation tests for images

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211191338) do
+ActiveRecord::Schema.define(version: 20180211233806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "images", force: :cascade do |t|
+    t.text "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
