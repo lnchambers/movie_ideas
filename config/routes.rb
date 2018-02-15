288430243095b9960b902aa84264d03dcbbde10d
Luke Chambers <lukenatchambers@gmail.com>
2018-02-14 17:49:14 -0700

    Fix admin/dashboard bugs

--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,7 @@ Rails.application.routes.draw do
 
   get "/admin/dashboard", to: "admin/dashboard#index"
 
+  post "/images", to: "images#create"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/features/users/admin_can_see_the_admin_dashboard_spec.rb
+++ spec/features/users/admin_can_see_the_admin_dashboard_spec.rb
@@ -5,7 +5,8 @@ describe "As an Admin" do
     it "I can create an image" do
       admin = User.new(username: "Opakawagalaga", email: "Bwing", password: "#notmypassword", role: 1)
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
-      visit movies_path(admin)
+      allow_any_instance_of(Admin::BaseController).to receive(:current_user).and_return(admin)
+      visit movies_path
 
       click_on "Admin Dashboard"
 
